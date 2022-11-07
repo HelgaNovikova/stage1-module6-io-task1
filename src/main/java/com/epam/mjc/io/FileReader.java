67c8b36db1c06a65594a1a7bf908a2b7e3c56f71
HelgaNovikova <helpotaki@rambler.ru>
2022-11-07 12:53:14 -0500

    st1m6t1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -2,9 +2,7 @@ package com.epam.mjc.io;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-
 
 
 public class FileReader {
@@ -13,25 +11,21 @@ public class FileReader {
         String name = "";
         int age = 0;
         String email = "";
-        Long phone = 0l;
+        long phone = 0L;
 
         try (FileInputStream fileI = new FileInputStream(file)) {
-            int i = 0;
-            String l = "";
+            int i;
+            StringBuilder l = new StringBuilder();
             while ((i = fileI.read()) != -1) {
-                l = l + (char) i;
+                l.append((char) i);
             }
-            System.out.println(l);
-            String[] profile = l.split(System.lineSeparator());
+            String[] profile = l.toString().split(System.lineSeparator());
             name = profile[0].substring(6);
             age = Integer.parseInt(profile[1].substring(5));
             email = profile[2].substring(7);
             phone = Long.parseLong(profile[3].substring(7));
-           // System.out.println(phone);
-        } catch (FileNotFoundException e) {
-            System.out.println("File wasn't found");
         } catch (IOException e) {
-            System.out.println("Smth went wrong");
+            e.printStackTrace();
         }
         return new Profile(name, age, email, phone);
     }
